c48fc3c0e41df7cd511021a185e3fa1687c6dd21
Poorna-A <apurna1998@gmail.com>
2021-03-03 21:52:35 +0530

    cmpltd

--- reversefn.py
+++ reversefn.py
@@ -1,12 +1,4 @@
-def rev(a):
-    
-    rev=0
-    rem=0
-    while(a != 0):
-        rem=a%10
-        rev=(rev*10)+rem
-        a=a//10
-    return rev 
+from reversemodule import rev
 a=int(input("enter a number"))
 result=rev(a)
 print(result)
---
+++ test.py
@@ -0,0 +1,7 @@
+from addmodule import add,sub
+
+a=45
+b=46
+
+result=sub(a,b)
+print(result)
